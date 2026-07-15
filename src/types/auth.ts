6940841eda890b13d0301a7cc00444c9958faf72
Federico Gentili <federicogentili05@gmail.com>
2026-07-15 19:44:52 +0200

    add auth context

---
+++ src/types/auth.ts
@@ -0,0 +1,7 @@
+export interface AuthContextType {
+  token: string | null
+  isAuthenticated: boolean
+  login: (username: string, password: string) => Promise<void>
+  logout: () => void
+  error: string | null
+}
